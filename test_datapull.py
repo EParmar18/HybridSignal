4ee42bae2843720a7c94b7ad84cf2bf48716d4d4
nicknazari <nhn2915@gmail.com>
2021-10-08 15:31:25 -0400

    added a test data pull

---
+++ test_datapull.py
@@ -0,0 +1,25 @@
+import requests
+import config
+
+#daily prices endpoint
+
+#define endpoint
+ticker = 'GOOG'
+endpoint = r"https://api.tdameritrade.com/v1/marketdata/{}/pricehistory".format(ticker);
+
+#define payload
+payload = {'apikey':config.client_id,
+	   'periodType':'day',
+	   'frequencyType':'minute',
+	   'frequency':'1',
+	   'period':'2',
+#	   'endDate':'1633721305000',
+#	   'startDate':'1554535854123',
+	   'needExtendedHours':'true'}
+
+#send the request
+content = requests.get(url = endpoint, params = payload)
+
+# convert the json string to a dictionary
+data = content.json()
+print(data)
