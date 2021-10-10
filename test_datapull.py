c3ac29b910b11125689e9757fab6f1ba95fd316f
Eshan Parmar <eshan.parmar@gmail.com>
2021-10-10 17:40:43 -0400

    Add RSI indicator and MACD Signal

--- test_datapull.py
+++ test_datapull.py
@@ -1,7 +1,7 @@
 import requests
 import config
 import pandas as pd
-import talib as ta
+#import talib as ta
 import matplotlib.pyplot as plt
 import pandas_ta as pta
 
@@ -38,21 +38,29 @@ x['8EMA'] = x['close'].ewm(span=8, adjust=False).mean()
 x['21EMA'] = x['close'].ewm(span=21, adjust=False).mean()
 
 x['MACD'] = x['21EMA'] - x['8EMA']
+x['MACDsignal'] = x['MACD'].ewm(span=9, adjust=False).mean()
 
 x['RSI'] = pta.rsi(x['close'], length = 14)
 
 print(x)
 
-fig, axes = plt.subplots(nrows=2, ncols=1, figsize=(10,7))
+fig, axes = plt.subplots(nrows=3, ncols=1, figsize=(10,7))
 
 x['zeroindex'] = 0
+x['30rsi'] = 30
+x['70rsi'] = 70
 
 x['MACD'].plot(title='MACD', label='macd', color='red', ax = axes[0])
+x['MACDsignal'].plot(label='macdsignal', color='green',ax = axes[0])
 x['zeroindex'].plot(color='black', ax = axes[0])
 
 x['8EMA'].plot(title='SIGNAL', label='8ema', color='blue', ax = axes[1])
 x['21EMA'].plot(title='SIGNAL', label='21ema', color='orange', ax = axes[1])
 #x['close'].plot(title='SIGNAL', label='price', color='purple', ax = axes[1])
+
+x['RSI'].plot(title='RSI', label='RSI', color='purple', ax = axes[2])
+x['30rsi'].plot(label='30', color='black', linestyle='dashed', ax = axes[2])
+x['70rsi'].plot(label='70', color='black', linestyle='dashed', ax=axes[2])
 
 
 plt.show()
